80795d0b9d59d640965606559b7862af1148b860
hhaaqqii <101864448+hhaaqqii@users.noreply.github.com>
2022-04-08 14:12:56 +0700

    Create TaskOverriding.java

---
+++ TaskOverriding.java
@@ -0,0 +1,20 @@
+public class Nama{
+ public void Sapa(){
+  System.out.println("Haqi Budi");
+ }
+ public static void main(String [] args){
+  Siapa ss;
+  Nama nn;
+  ss = new Siapa();
+  nn = new Nama();
+  
+  ss.Sapa();//memanggil method Sapa() pada class Siapa
+  nn.Sapa();//memanggil method Sapa() pada class Nama
+ }
+}
+class Siapa extends Nama{
+ //Method Sapa() pada class Nama di override
+ public void Sapa(){
+  System.out.println("Haqi Ramadhita");
+ }
+}
